5f5ee6ff0a6bf88b7c2fd214639e2fc264f710d2
truongchau9999 <chaunhattruong9999@gmail.com>
2022-03-01 15:47:53 +0700

    first commit python code

---
+++ 02_comment.py
@@ -0,0 +1,11 @@
+# This is comment 1 line
+# Print statement
+print("Hello World!");
+
+'''
+
+This is comment multi lines
+one line
+two line
+
+'''
---
+++ 03_variable.py
@@ -0,0 +1,4 @@
+x=5
+y="truong"
+print(x)
+print(y)
